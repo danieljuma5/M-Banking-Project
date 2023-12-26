8f4330ace20c2fcc9c32145d4d6926e489c651db
danieljuma5 <jumadaniel275@gmail.com>
2023-12-26 23:39:15 +0300

    Wrapped application in navigation container element

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <Text>The M-Banking application begins!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
