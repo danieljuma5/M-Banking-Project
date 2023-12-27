7b92b5a1a693410c74f930fb6dc2d3c20d8c855e
danieljuma5 <jumadaniel275@gmail.com>
2023-12-27 23:24:34 +0300

    Added various services links

--- src/screens/BankServicesScreen.js
+++ src/screens/BankServicesScreen.js
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const BankServices = () => {
+  const navigation = useNavigation();
+
+  const handleDeposit = () => {
+    // Navigation or logic for Deposit service
+    // For example: navigation.navigate('DepositScreen');
+  };
+
+  const handleBalanceEnquiry = () => {
+    // Navigation or logic for Balance Enquiry service
+    // For example: navigation.navigate('BalanceEnquiryScreen');
+  };
+
+  const handleStopCheque = () => {
+    // Navigation or logic for Stop Cheque service
+    // For example: navigation.navigate('StopChequeScreen');
+  };
+
+  const handleChangePin = () => {
+    // Navigation or logic for Change PIN service
+    // For example: navigation.navigate('ChangePinScreen');
+  };
+
+  const handleFinancialServices = () => {
+    // Navigation or logic for Financial Services
+    // For example: navigation.navigate('FinancialServicesScreen');
+  };
+
   return (
-    <View>
-      <Text>BankServices</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.serviceLink} onPress={handleDeposit}>
+        <Text>Deposit</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.serviceLink} onPress={handleBalanceEnquiry}>
+        <Text>Balance Enquiry</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.serviceLink} onPress={handleStopCheque}>
+        <Text>Stop Cheque</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.serviceLink} onPress={handleChangePin}>
+        <Text>Change PIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.serviceLink} onPress={handleFinancialServices}>
+        <Text>Financial Services</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default BankServices
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  serviceLink: {
+    padding: 10,
+    marginVertical: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default BankServices;
