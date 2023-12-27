e846f4f33f7efd36639662aaa45f2a27f945e299
danieljuma5 <jumadaniel275@gmail.com>
2023-12-27 17:47:50 +0300

    Added Login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,20 +1,73 @@
-// LoginScreen.js
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const LoginScreen = ({ navigation }) => {
-  const handleLogin = () => {
-    // Logic for handling login
-    // Navigate to the home/dashboard upon successful login
-    navigation.navigate('Home'); // Replace 'Home' with your desired screen name
+const Login = () => {
+  const navigation = useNavigation();
+
+  const handleExit = () => {
+    // Code to exit the app
+  };
+
+  const handleRegister = () => {
+    // Code to navigate to the signup page (to be implemented later)
+  };
+
+  const handleSubmit = () => {
+    // Logic for handling form submission (login)
   };
 
   return (
-    <View>
-      <Text>Login Screen</Text>
-      <Button title="Login" onPress={handleLogin} />
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Account Number"
+          // onChangeText={(text) => handleAccountNumberChange(text)}
+          // value={accountNumber}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          // onChangeText={(text) => handlePasswordChange(text)}
+          // value={password}
+        />
+        <Button title="Submit" onPress={handleSubmit} />
+      </View>
+      <View style={styles.footer}>
+        <Button title="Exit" onPress={handleExit} />
+        <Button title="Register" onPress={handleRegister} />
+      </View>
     </View>
   );
 };
 
-export default LoginScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  formContainer: {
+    width: '100%',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+});
+
+export default Login;
