154b589a614c18d420e2ad330c29ba93394e8e0f
danieljuma5 <jumadaniel275@gmail.com>
2023-12-27 18:09:37 +0300

    Added exit functionality to Login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, BackHandler, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Constants from 'expo-constants';
 
 const Login = () => {
   const navigation = useNavigation();
+  const [showExitModal, setShowExitModal] = useState(false);
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+      if (showExitModal) {
+        setShowExitModal(false);
+        return true;
+      }
+      setShowExitModal(true);
+      return true;
+    });
+
+    return () => backHandler.remove();
+  }, [showExitModal]);
+
+  const handleExitConfirmation = () => {
+    Alert.alert(
+      'Exit Application',
+      'Are you sure you want to exit the application?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => setShowExitModal(false),
+          style: 'cancel',
+        },
+        { text: 'Yes', onPress: () => handleExit() },
+      ],
+      { cancelable: true }
+    );
+  };
 
   const handleExit = () => {
-    // Code to exit the app
+    BackHandler.exitApp();
   };
 
   const handleRegister = () => {
@@ -35,8 +66,15 @@ const Login = () => {
         />
         <Button title="Submit" onPress={handleSubmit} />
       </View>
+      {showExitModal && (
+        <View style={styles.exitModal}>
+          <Text style={styles.modalText}>Are you sure you want to exit the application?</Text>
+          <Button title="Yes" onPress={handleExit} />
+          <Button title="No" onPress={() => setShowExitModal(false)} />
+        </View>
+      )}
       <View style={styles.footer}>
-        <Button title="Exit" onPress={handleExit} />
+        <Button title="Exit" onPress={handleExitConfirmation} />
         <Button title="Register" onPress={handleRegister} />
       </View>
     </View>
@@ -67,6 +105,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
+  },
+  exitModal: {
+    position: 'absolute',
+    top: '40%',
+    left: '15%',
+    width: '70%',
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 10,
+    textAlign: 'center',
   },
 });
 
